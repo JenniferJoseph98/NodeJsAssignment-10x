73404c20e2ac61e9e168edd961e061aae228b391
JenniferJoseph98 <113026977+JenniferJoseph98@users.noreply.github.com>
2022-11-16 00:17:22 +0530

    Update index.js

--- Assignment-4/index.js
+++ Assignment-4/index.js
@@ -8,7 +8,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs'); 
 app.get('/', (req,res)=>{
     // console.log(res)
-    res.send("hello Chavva!")
+    res.send("hello jennifer!")
    
 })
 
